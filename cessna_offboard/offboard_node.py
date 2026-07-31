fc7eac15920a5a8317cc16df75d5121e0a592e84
Rowan Evans <raevans2009@gmail.com>
2026-07-30 22:49:08 -0400

    rebuilding package saving code

--- cessna_offboard/offboard_node.py
+++ cessna_offboard/offboard_node.py
@@ -3,7 +3,7 @@ import numpy as np
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSDurabilityPolicy, QoSHistoryPolicy
 
-from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleStatus
+from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleOdometry
 
 
 class OffboardNode(Node):
@@ -20,60 +20,72 @@ class OffboardNode(Node):
             depth = 0 # queue size -> only if KEEP_LAST
         )
 
+        # subscriber qos profile
         qos_sub = QoSProfile(
             reliability = QoSReliabilityPolicy.BEST_EFFORT,
             durability = QoSDurabilityPolicy.VOLATILE, # makes no attempt to persist
             history=QoSHistoryPolicy.KEEP_LAST,
-            depth = 2
+            depth = 1
         )
 
+        # subscribes to status to get real time updates of position, etc.
         self.status_sub = self.create_subscription(
-            VehicleStatus,
-            'fmu/out/vehicle_status',
+            VehicleOdometry,
+            'fmu/out/vehicle_odometry',
             self.status_callback,
             qos_sub
         )
 
-        self.status_sub = self.create_subscription(
-            VehicleStatus,
-            'fmu/out/vehicle_status_v1',
-            self.status_callback,
-            qos_sub
-        )
-
+        # creates the three publishers
         self.offboard_pub = self.create_publisher(OffboardControlMode, 'fmu/in/offboard_control_mode', qos_pub)
         self.trajectory_pub = self.create_publisher(TrajectorySetpoint, 'fmu/in/trajectory_setpoint', qos_pub)
         self.command_pub = self.create_publisher(VehicleCommand, 'fmu/in/vehicle_command', qos_pub)
 
         timer_period = 0.1
 
+        #main timer
         self.main_timer = self.create_timer(timer_period, self.main_callback) 
 
-        self.vehicle_status = VehicleStatus()
+        #gets initial status
+        #self.vehicle_status = VehicleStatus()
 
         self.dt = timer_period 
         self.declare_parameter('radius', 10.0) # radius of orbiting circle
         self.declare_parameter('omega', 5.0) # angular velocity magnitude
-        self.declare_parameter('altitude', 5) # altitude of orbiting circle
 
-        self.nav_state = VehicleStatus.NAVIGATION_STATE_MAX
-        self.arming_state = VehicleStatus.ARMING_STATE_DISARMED
+        self.x = 0.0 # x position
+        self.y = 0.0 # y position
+        self.z_increment = 1.0
+        self.current_z = 0.0 # z position
+        self.new_z = 0.0
+        self.target_z = 10.0 # target z position
 
+
+        #parameters to update live
         self.theta = 0.0 # angle going around the orbiting cirlce
         self.radius = self.get_parameter('radius').value
         self.omega = self.get_parameter('omega').value
-        self.altitude = self.get_parameter('altitude').value
 
         self.offboard_count = 0
 
+    #status callback every time a status gets published
     def status_callback(self, msg):
-        self.get_logger().info("receiving status")
-        print("nav status: ", msg.nav_state)
-        print(" | offboard status: ", VehicleStatus.NAVIGATION_STATE_OFFBOARD)
-        self.nav_state = msg.nav_state
-        self.arming_state = self.arming_state
+        #get new position
+        self.x = msg.position[0]
+        self.y = msg.position[1]
+        self.current_z = msg.position[2]
+
+        #calculate new z value
+        if self.current_z >= self.target_z:
+            self.new_z = self.current_z + self.z_increment
+        else:
+            self.new_z = self.current_z - self.z_increment
+
+        #publish what the altitude is
+        self.get_logger().info(f"altitude:{(self.current_z * -1):.2f}")
 
 
+    #main callback function
     def main_callback(self):
         if self.offboard_count == 10:
             self.offboard()
@@ -93,11 +105,11 @@ class OffboardNode(Node):
         if self.offboard_count <= 11:
             self.offboard_count += 1
 
-        if (self.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD and self.arming_state == VehicleStatus.ARMING_STATE_ARMED):
+        if self.offboard_count == 10:
             trajectory_msg = TrajectorySetpoint()
             trajectory_msg.position[0] = self.radius * np.cos(self.theta)
             trajectory_msg.position[1] = self.radius * np.sin(self.theta)
-            trajectory_msg.position[2] = -self.altitude
+            trajectory_msg.position[2] = -self.new_z
             self.trajectory_pub.publish(trajectory_msg)
 
             self.theta = self.theta + self.omega * self.dt

dcc2c924516ba33fc332c27d04b2c0eb7bdb1b61
Rowan Evans <raevans2009@gmail.com>
2026-09-19 21:25:36 -0400

    added gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'cessna_offboard'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -25,7 +28,8 @@ setup(
     entry_points={
         'console_scripts': [
             'offboard = cessna_offboard.offboard_node:main',
-            'quadcopter = cessna_offboard.quadcopter_offboard:main'
+            'quadcopter = cessna_offboard.quadcopter_offboard:main',
+            'scene_markers = cessna_offboard.scene_markers:main',
         ],
     },
 )
